7e4059abefcd34310f2ce4c9e8186357289686ed
emvherrera <emilyvherrera98@gmail.com>
2025-04-16 13:34:35 -0400

    commit BasicCalculator

---
+++ ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("orange");
+        System.out.println("tomato");
+        System.out.println("lime");
+        System.out.println("eggs");
+        System.out.println("milk");
+        System.out.println("cheese");
+        System.out.println("celery");
+        System.out.println("tuna");
+        System.out.println("bread");
+        System.out.println("cucumber");
+        System.out.println("grapes");
+
+    }
+}
